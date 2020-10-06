d4de8ed8c1e0eb38d02accd75a6a62d4c48b320d
SevenMoor <au.ottaviano@yahoo.com>
2020-10-06 18:59:04 +0200

    Bluetooth to debug

---
+++ app/src/main/java/com/sevenmoor/penstriman/DiscoverActivity.java
@@ -0,0 +1,117 @@
+package com.sevenmoor.penstriman;
+
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ScrollView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class DiscoverActivity extends AppCompatActivity {
+
+    ScrollView scrollView;
+    BluetoothAdapter adapter;
+    FloatingActionButton refresh;
+
+
+    BroadcastReceiver discoveryMonitor = new BroadcastReceiver() {
+        String dStarted = BluetoothAdapter.ACTION_DISCOVERY_STARTED;
+        String dFinished = BluetoothAdapter.ACTION_DISCOVERY_FINISHED;
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (dStarted.equals(intent.getAction())) {
+                Toast.makeText(getApplicationContext(), "Discovery Started. . .", Toast.LENGTH_SHORT).show();
+            } else if (dFinished.equals(intent.getAction())) {
+                Toast.makeText(getApplicationContext(), "Discovery Completed. . .", Toast.LENGTH_SHORT).show();
+            }
+        }
+    };
+
+    BroadcastReceiver adapterState = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String stateExtra = BluetoothAdapter.EXTRA_STATE;
+            int state = intent.getIntExtra(stateExtra, -1);
+            String tt = "";
+            switch (state) {
+                case (BluetoothAdapter.STATE_TURNING_ON): {
+                    tt = "Bluetooth turning on";
+                    break;
+                }
+                case (BluetoothAdapter.STATE_ON): {
+                    tt = "Bluetooth on";
+                    unregisterReceiver(this);
+                    break;
+                }
+                case (BluetoothAdapter.STATE_TURNING_OFF): {
+                    tt = "Bluetooth turning off";
+                    break;
+                }
+                case (BluetoothAdapter.STATE_OFF): {
+                    tt = "Bluetooth off";
+                    break;
+                }
+                default:
+                    break;
+            }
+            Toast.makeText(getApplicationContext(), tt, Toast.LENGTH_SHORT).show();
+
+        }
+    };
+
+    BroadcastReceiver discoveryResult = new BroadcastReceiver(){
+        @Override
+        public void onReceive(Context context, Intent intent){
+            BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+            TextView view = new TextView(scrollView.getContext());
+            view.setText(device.getName());
+            scrollView.addView(view);
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.discover);
+
+        scrollView = findViewById(R.id.scrollview);
+        adapter = BluetoothAdapter.getDefaultAdapter();
+        refresh = findViewById(R.id.refreshButton);
+
+        String enableRequest = BluetoothAdapter.ACTION_REQUEST_ENABLE;
+        startActivityForResult(new Intent(enableRequest), 0);
+
+        if (!adapter.isEnabled()) {
+            String actionStateChanged = BluetoothAdapter.ACTION_STATE_CHANGED;
+            String actionRequestEnable = BluetoothAdapter.ACTION_REQUEST_ENABLE;
+            registerReceiver(adapterState, new IntentFilter(actionStateChanged));
+            startActivityForResult(new Intent(actionRequestEnable), 0);
+        }
+
+        refresh.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                boolean result = adapter.startDiscovery();
+                Log.i("Info", "adapter state int="+adapter.getState());
+                Log.i("Info", "Discovering="+result);
+            }
+        });
+
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(BluetoothAdapter.ACTION_DISCOVERY_STARTED);
+        filter.addAction(BluetoothAdapter.ACTION_DISCOVERY_FINISHED);
+        registerReceiver(discoveryMonitor,filter);
+        registerReceiver(discoveryResult, new IntentFilter(BluetoothDevice.ACTION_FOUND));
+    }
+}
--- app/src/main/java/com/sevenmoor/penstriman/MainActivity.java
+++ app/src/main/java/com/sevenmoor/penstriman/MainActivity.java
@@ -2,7 +2,9 @@ package com.sevenmoor.penstriman;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ScrollView;
 import android.widget.SearchView;
 import android.widget.Toast;
@@ -23,6 +25,13 @@ public class MainActivity extends AppCompatActivity {
         floatingActionButton = findViewById(R.id.floatingActionButton);
         searchView = findViewById(R.id.searchView);
         scrollView = findViewById(R.id.scrollview);
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),DiscoverActivity.class);
+                startActivity(intent);
+            }
+        });
 
         searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
