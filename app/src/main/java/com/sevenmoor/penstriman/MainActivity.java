c47cf447e719b3f2714cf9140dcdfb0dde5cf9b5
mahff <mahfoudamhiyen@live.fr>
2020-10-06 10:35:16 +0200

    Creation of activities + begin main activity_

--- app/src/main/java/com/sevenmoor/penstriman/MainActivity.java
+++ app/src/main/java/com/sevenmoor/penstriman/MainActivity.java
@@ -3,12 +3,44 @@ package com.sevenmoor.penstriman;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ScrollView;
+import android.widget.SearchView;
+import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    FloatingActionButton floatingActionButton;
+    SearchView searchView;
+    ScrollView scrollView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        floatingActionButton = findViewById(R.id.floatingActionButton);
+        searchView = findViewById(R.id.searchView);
+        scrollView = findViewById(R.id.scrollview);
+        
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                search(query);
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                return false;
+            }
+
+
+        });
+    }
+
+    private void search(String text){
+        Toast.makeText(this, text, Toast.LENGTH_SHORT).show();
     }
 }
